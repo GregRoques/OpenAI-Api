7babe528b2b27bd5dbd15a6c2e2a9b2f98c02ac7
Greg Roques <42392687+GregRoques@users.noreply.github.com>
2023-03-24 16:56:25 -0400

    Update getChatGPT.js

--- backend/getChatGPT.js
+++ backend/getChatGPT.js
@@ -13,19 +13,65 @@ const openAi = new OpenAIApi(
 
   // =================== script
 
-  export default router.post("/", async (req, res) => {
+  export default router.post("/", (req, res) => {
     try{
 
-        const data = req.body.value;
-        
-        const response = await openAi.createChatCompletion({
+        const data = req.body.request;
+        const type = req.body.request_type.toLowerCase();
+
+        if(type == "image"){
+          openAi.createImage({
+            prompt: data,
+            n: 1,
+            size: "1024x1024"
+          }).then(imgRes=>{
+            const newImage = imgRes.data.data[0].url;
+            console.log(newImage)
+            return res.json(newImage)
+          }).catch(err=>{
+            throwError(err)
+          })
+        }
+        if(type == "code"){
+          openAi.c
+          openAi.createCompletion({
+            model: "text-davinci-002",
+            prompt: data
+          }).then(codeRes=>{
+            const newCode = codeRes.data.choices[0].text;
+            console.log(newCode)
+            return res.json(newCode)
+          }).catch(err=>{
+            throwError(err)
+          })
+        }
+        if(type == "chat"){
+          openAi.createChatCompletion({
             model: "gpt-3.5-turbo",
             messages: [{ role: "user", content: data}],
-        });
-        const getResponse = response.data.choices[0].message.content
-        console.log(getResponse)
-        return res.json(getResponse);
+          }).then(chatRes=>{
+            const newChat = chatRes.data.choices[0].message.content;
+            console.log(newChat)
+            return res.json(newChat)
+          }).catch(err=>{
+            throwError(err)
+          })
+        }
+      
     } catch(err){
-        throw new Error(err);
+        throwError(err)
+    }
+
+    function throwError(err){
+      let iAmError;
+      if(err.response){
+        const {status, data} = err.response;
+        iAmError = `${status}: ${data}`;
+      }else{
+        iAmError = err
+      }
+      console.log(iAmError);
+      throw new Error(iAmError)  
     }
   })
+
